585e60fe6e11fdebd1badd8957582c7e0d1e816b
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2023-01-21 20:49:39 +0100

    Add percent, clear, clear entry and delete

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ The Odin Project: Calculator
 
 Final project from the Foundations section from The Odin Project.
 Details can be found here: https://www.theodinproject.com/lessons/foundations-calculator
+
+From the basic proposal in the assignment, I took the liberty of deviating regarding the possibility of stringing together several operations, as it goes against the most common design in simple calculators.
+Instead, when the user clicks an operator after having already 2 operands in place, the calculator will resolve the pending operation before committing to the new one, so that the user is always focused on the current status of the calculation.
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let currentOperand1 = '';
 let currentOperand2 = '';
 let currentOperator = '';
+let previousOperand1 = '';
+let previousOperand2 = '';
+let previousOperator = '';
 
 function updateDisplay() {
-    const displayField = document.querySelector('#display');
-    displayField.value = `${currentOperand1} ${currentOperator} ${currentOperand2}`;
+    const displayField = document.querySelector('.display');
+    displayField.innerHTML = `${currentOperand1} ${currentOperator} ${currentOperand2}`;
+    const displayHistoryField = document.querySelector('.display-history');
+    displayHistoryField.innerHTML = `${previousOperand1} ${previousOperator} ${previousOperand2}`;
 }
 
 function addToOperand(input) {
@@ -19,7 +24,7 @@ function addToOperand(input) {
 } 
 
 function setOperator(operator) {
-    const unaryOperators = ["1/x", "x²", "√x"];
+    const unaryOperators = ["1/x", "x²", "√x", "%"];
     if (unaryOperators.includes(operator)) {
         //Calculates directly if the operator is a unary operator.
         currentOperator = operator;
@@ -60,6 +65,41 @@ function squareRoot(a) {
     return Math.sqrt(a);
 }
 
+function percent(a) {
+    return a/100;
+}
+
+function clearEntry() {
+    if (currentOperand2 != '') {
+        currentOperand2 = '';
+    } else {
+        currentOperand1 = '';
+        currentOperator = '';
+    }
+    updateDisplay();
+}
+
+function clear() {
+    currentOperand1 = '';
+    currentOperand2 = '';
+    currentOperator = '';
+    previousOperand1 = '';
+    previousOperand2 = '';
+    previousOperator = '';
+    updateDisplay();
+}
+
+function del() {
+    if (currentOperand2 != '') {
+        currentOperand2 = currentOperand2.substring(0, (currentOperand2.length -1));
+    } else if (currentOperator != '') {
+        currentOperator = '';
+    } else {
+        currentOperand1 = currentOperand1.substring(0, (currentOperand1.length -1));
+    }
+    updateDisplay();
+}
+
 function operate(a, b, operator){
     switch (operator) {
         case '+':
@@ -76,12 +116,17 @@ function operate(a, b, operator){
             return square(a);
         case "√x":
             return squareRoot(a);
+        case "%":
+            return percent(a)
         default:
             return 'Error!'
     }
 }
 
 function calculateResult() {
+    previousOperand1 = currentOperand1;
+    previousOperand2 = currentOperand2;
+    previousOperator = currentOperator;
     if (currentOperator == '/' && currentOperand2 == '0') {
         alert('Generating black hole, please wait...');
         currentOperand1 = '';
@@ -97,22 +142,31 @@ function calculateResult() {
 }
     
 function setup() {
-    let operandButtons = Array.from(document.querySelectorAll('.operand'));
+    const operandButtons = Array.from(document.querySelectorAll('.operand'));
     operandButtons.forEach(operandButton => {
         operandButton.addEventListener("click", () => {
             addToOperand(operandButton.value);
         });
     });
 
-    let operatorButtons = Array.from(document.querySelectorAll('.operator'));
+    const operatorButtons = Array.from(document.querySelectorAll('.operator'));
     operatorButtons.forEach(operatorButton => {
         operatorButton.addEventListener("click", () => {
             setOperator(operatorButton.value);
         });
     });
 
-    let resultButton = document.querySelector('.result');
+    const resultButton = document.querySelector('.result');
     resultButton.addEventListener("click", calculateResult);
+
+    const clearEntryButton = document.querySelector('#clear-entry');
+    clearEntryButton.addEventListener("click", clearEntry);
+
+    const clearButton = document.querySelector('#clear');
+    clearButton.addEventListener("click", clear);
+
+    const deleteButton = document.querySelector('#delete');
+    deleteButton.addEventListener("click", del);
 }
 
 setup()
